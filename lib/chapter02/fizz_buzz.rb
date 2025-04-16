f45503f27ee9d6825d985362173d8d59f1cac7d2
Ryoh Akiyoshi <ryo614@gmail.com>
2025-04-16 14:34:15 +0900

    chapter02

---
+++ lib/chapter02/fizz_buzz.rb
@@ -0,0 +1,27 @@
+def fizz_buzz(number)
+  if (number % 15).zero?
+    puts 'FizzBuzz'
+  elsif (number % 3).zero?
+    puts 'Fizz'
+  elsif (number % 5).zero?
+    puts 'Buzz'
+  else
+    puts n
+  end
+end
+
+fizz_buzz(1)
+fizz_buzz(2)
+fizz_buzz(3)
+fizz_buzz(4)
+fizz_buzz(5)
+fizz_buzz(6)
+fizz_buzz(7)
+fizz_buzz(8)
+fizz_buzz(9)
+fizz_buzz(10)
+fizz_buzz(11)
+fizz_buzz(12)
+fizz_buzz(13)
+fizz_buzz(14)
+fizz_buzz(15)
